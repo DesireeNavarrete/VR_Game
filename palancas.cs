5b865421a37d740438ff5e08ca5c99edcf689e12
DesireeNavarrete <desiree.nav96@gmail.com>
2019-12-19 12:23:46 +0100

    Add files via upload

---
+++ palancas.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class palancas : MonoBehaviour
+{
+    public GameObject pal1, pal2, pal3, pal4, pal5, pal6, pal7, pal8, pal9, pal10;
+    Animator animPal,animPal2, animPal3, animPal4, animPal5, animPal6, animPal7, animPal8, animPal9, animPal10;
+    bool arriba1, arriba2, arriba3, arriba4, arriba5, arriba6, arriba7, arriba8, arriba9, arriba10;
+    public Light luzRoja, luzAma, luzAzul, luzVerde,luzGeneral;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        animPal =pal1.GetComponent<Animator>();
+        arriba1 = true;
+
+        animPal2 = pal2.GetComponent<Animator>();
+        arriba2 = true;
+        
+        animPal3 = pal3.GetComponent<Animator>();
+        arriba3 = true;
+
+        animPal4 = pal4.GetComponent<Animator>();
+        arriba4 = true;
+
+        animPal5 = pal5.GetComponent<Animator>();
+        arriba5 = true;
+
+        animPal6 = pal6.GetComponent<Animator>();
+        arriba6 = true;
+
+        animPal7 = pal7.GetComponent<Animator>();
+        arriba7 = true;
+
+        animPal8 = pal8.GetComponent<Animator>();
+        arriba8 = true;
+
+        animPal9 = pal9.GetComponent<Animator>();
+        arriba9 = true;
+
+        animPal10 = pal10.GetComponent<Animator>();
+        arriba10 = true;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (arriba1&&arriba2==false&&arriba3==false&&arriba4&&arriba5==false)//palancas correctas para comprobar de la primera parte
+        {
+            print("correcto");
+            luzRoja.enabled = false;
+        }
+        else
+            print("incorrecto");
+
+
+        if (arriba6 && arriba7 && arriba8 == false && arriba9 && arriba10)//palancas correctas para comprobar de la segunda parte
+        {
+            print("correcto2");
+            luzAma.enabled = false;
+            luzAzul.enabled = false;
+            luzVerde.enabled = false;
+            luzGeneral.enabled = true;
+
+
+
+
+        }
+        else
+            print("incorrecto2");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //primera palanca
+        if (other.transform.tag == "pal1") {
+        print("Tocado palanca " + other.gameObject.name);
+        animPal.SetTrigger("palanca1");
+        arriba1 = !arriba1;
+        }
+
+        //segunda palanca
+        if (other.transform.tag == "pal2")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal2.SetTrigger("palanca2");
+            arriba2 = !arriba2;
+        }
+
+        //tercera palanca
+        if (other.transform.tag == "pal3")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal3.SetTrigger("palanca3");
+            arriba3 = !arriba3;
+        }
+
+        //cuarta palanca
+        if (other.transform.tag == "pal4")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal4.SetTrigger("palanca4");
+            arriba4 = !arriba4;
+        }
+
+        //quinta palanca
+        if (other.transform.tag == "pal5")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal5.SetTrigger("palanca5");
+            arriba5 = !arriba5;
+        }
+
+        //sexta palanca
+        if (other.transform.tag == "pal6")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal6.SetTrigger("palanca6");
+            arriba6 = !arriba6;
+        }
+        //septima palanca
+        if (other.transform.tag == "pal7")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal7.SetTrigger("palanca7");
+            arriba7 = !arriba7;
+        }
+        //quinta palanca
+        if (other.transform.tag == "pal8")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal8.SetTrigger("palanca8");
+            arriba8 = !arriba8;
+        }
+        //octava palanca
+        if (other.transform.tag == "pal9")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal9.SetTrigger("palanca9");
+            arriba9 = !arriba9;
+        }
+        //decima palanca
+        if (other.transform.tag == "pal10")
+        {
+            print("Tocado palanca " + other.gameObject.name);
+            animPal10.SetTrigger("palanca10");
+            arriba10 = !arriba10;
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class player : MonoBehaviour
+{
+
+    //public int[] codCorrecto = { 1, 3, 7, 9, 2, 4 };//array 
+    public int[] codCorrecto = { 1, 2,3,6,9,8 };//array 
+
+    public int[] codIntroducido;
+    //public GameObject num1,num2,num3,num4,num5,num6,num7,num8,num9,num0;
+    public int secuencia, num_cubo;
+        public int contador;
+
+    public Light led1, led2;
+    public Text num;
+    public GameObject puerta;
+    public bool cod1, cod2;
+    Animator caja;
+    //public int[] codCorrecto = { 1, 3, 7, 9, 2, 4 };//array 
+    public int[] codCorrectoColor = { 1, 1, 1, 1 };//array 
+
+    public int[] codIntroducidoColor;
+    //public GameObject num1,num2,num3,num4,num5,num6,num7,num8,num9,num0;
+    public int secuenciaColor, num_color;
+
+    //public Text numColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        contador = 0;
+        led1.enabled = false;
+        led2.enabled = false;
+        cod1 = false;
+        cod2 = false;
+        caja = puerta.GetComponent<Animator>();
+
+        caja.SetBool("open", false);
+
+        secuencia = 0;
+        num.text = "";
+
+        secuenciaColor = 0;
+        //numColor.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contador == 1)
+        {
+            contador = 0;
+        }
+        if (cod1 && cod2)
+        {
+            caja.SetBool("open", true);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "boton") { 
+        print("Tocado botón " + other.gameObject.name);
+        if (secuencia < 6 &&contador==0 )
+        {
+
+            num_cubo = System.Int32.Parse(other.gameObject.name);
+            codIntroducido[secuencia] = num_cubo;
+            secuencia++;
+            num.text += num_cubo;
+            contador = 1;
+            print(contador);
+
+        }
+        if (secuencia == 6)
+        {
+            for (int i = 0; i < codIntroducido.Length; i++)
+            {
+                if(codIntroducido[i] == codCorrecto[i])
+                {
+                    print("correcto");
+                        cod1 = true;
+                        led1.enabled = true;
+                    }
+                if(codIntroducido[i] != codCorrecto[i])
+                {
+                    print("incorrecto");
+                    num.text = "";
+                    secuencia = 0;
+                    for (int x = 0; x < codIntroducido.Length; x++)//borra el array
+                    {
+                        codIntroducido[x] = 0;
+
+                    }
+                }
+
+                }
+            }
+        }
+
+
+        if (other.transform.tag == "BotonesColor") { 
+        print("Tocado botón " + other.gameObject.name);
+        if (secuenciaColor < 4)
+        {
+
+            num_color = System.Int32.Parse(other.gameObject.name);
+            codIntroducidoColor[secuenciaColor] = num_color;
+            secuenciaColor++;
+            //num.text += num_cubo;
+            //print(secuencia +" ");
+
+        }
+        if (secuenciaColor == 4 && contador == 0)
+        {
+                for (int i = 0; i < codIntroducidoColor.Length; i++)
+                {
+                    if (codIntroducidoColor[i] == codCorrectoColor[i])
+                    {
+                        print("correcto");
+                        cod2 = true;
+                        led2.enabled = true;
+                        num.text = "";
+                        contador = 1;
+
+                    }
+                    if (codIntroducidoColor[i] != codCorrectoColor[i])
+                    {
+                        print("incorrecto");
+                        //numColor.text = "";
+                        secuenciaColor = 0;
+                        for (int x = 0; x < codIntroducidoColor.Length; x++)//borra el array
+                        {
+                            codIntroducidoColor[x] = 0;
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    }
+
